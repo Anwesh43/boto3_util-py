f4844071f782c954dabc23bb9484a5110486c488
Anwesh43 <anweshthecool0@gmail.com>
2019-10-21 20:29:47 +0530

    subscribing to topic

--- sns_util.py
+++ sns_util.py
@@ -14,6 +14,18 @@ def deleteTopic(sns, topicArn):
     response = sns.delete_topic(TopicArn = topicArn)
     return response
 
+def createSubscription(sns, topicArn, protocol, endpoint):
+    response = sns.subscribe(TopicArn = topicArn, Protocol = protocol, Endpoint = endpoint)
+    return response['SubscriptionArn']
+
+def listSubscriptionsByTopic(sns, topicArn):
+    response = sns.list_subscriptions_by_topic(TopicArn = topicArn)
+    return response['Subscriptions']
+
+def unsubscribe(sns, subscriptionArn):
+    response = sns.unsubscribe(SubscriptionArn = subscriptionArn)
+    return response
+
 
 sns = clientCache.get('sns')
 arguments = sys.argv
@@ -22,10 +34,19 @@ oneArgMap = {}
 threeArgsMap = {}
 twoArgsMap['createTopic'] = createTopic
 twoArgsMap['deleteTopic'] = deleteTopic
+twoArgsMap['listSubs'] = listSubscriptionsByTopic
+twoArgsMap['unsubscribe'] = unsubscribe
 oneArgMap['listTopic'] = listTopic
+
+fourArgsMap = {}
+fiveArgsMap = {}
+fiveArgsMap['subscribe'] = createSubscription
 
 if len(arguments) == 2:
     print(oneArgMap[arguments[1]](sns))
 
 if len(arguments) == 3:
     print(twoArgsMap[arguments[1]](sns, arguments[2]))
+
+if len(arguments) == 5:
+    print(fiveArgsMap[arguments[1]](sns, arguments[2], arguments[3], arguments[4]))
